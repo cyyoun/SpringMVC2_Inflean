35ed01165ab1fafb703e817feb0935920fc11da0
cyyoun <cyyoun41@gmail.com>
2023-07-25 21:25:40 +0900

    Ch2.타임리프 - 셀렉트 박스

--- form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -35,7 +35,7 @@ public class ItemRepository {
         findItem.setOpen(updateParam.getOpen());
         findItem.setRegions(updateParam.getRegions());
         findItem.setItemType(updateParam.getItemType());
-        findItem.setItemName(updateParam.getItemName());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
