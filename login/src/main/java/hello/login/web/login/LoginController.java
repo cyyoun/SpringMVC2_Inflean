5e6431724b12f603a3f79ee1fd0f8ce70f2585e9
cyyoun <cyyoun41@gmail.com>
2023-08-01 11:05:31 +0900

    Ch07. 로그인 처리하기 (필터, 인터셉터) - Servlet Filter 인증체크

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -74,7 +75,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
 
         if (bindingResult.hasErrors()) {
@@ -98,6 +99,35 @@ public class LoginController {
 //        sessionManager.createSession(loginMember, response);
 
         return "redirect:/";
+    }
+
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        //글로벌 오류
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성 -> request.getSession(true) --> 기본 값이 true
+        HttpSession session = request.getSession(); //-> request.getSession(false) - 세션 없으면 null 반환
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        //세션 관리자를 통해 세션을 생성하고 회원 데이터 보관
+//        sessionManager.createSession(loginMember, response);
+
+        return "redirect:" + redirectURL;
     }
 
     //    @PostMapping("/logout")
