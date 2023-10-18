cb324c59efcf0dd00d17a990fe1c3f46d9881ffa
cyyoun <cyyoun41@gmail.com>
2023-10-18 12:20:33 +0900

    Ch8. 서블릿 예외 처리 - 오류 화면 제공

---
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        // 1번째 파라미터 예외 사항이 터지면 2번째 컨트롤러로 호출
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        //등록
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
